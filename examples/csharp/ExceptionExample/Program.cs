fbbb3672ad48309fe321f57ce23ff3cc0cfe7ec7
Kyle Machulis <kyle@machul.is>
2018-12-05 23:34:13 -0800

    Add csharp Exception example

---
+++ examples/csharp/ExceptionExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using Buttplug.Client;
+using Buttplug.Core;
+
+namespace ExceptionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In CSharp, we've got the 5 main exception classes, plus
+            // ButtplugClientConnectorException. All of these are children of ButtplugException, so
+            // catching ButtplugException will catch all Buttplug errors but let all other system
+            // Exceptions bubble up.
+            try
+            {
+
+            }
+            catch (ButtplugHandshakeException e)
+            {
+
+            }
+            catch (ButtplugDeviceException e)
+            {
+
+            }
+            catch (ButtplugMessageException e)
+            {
+
+            }
+            catch (ButtplugPingException e)
+            {
+
+            }
+            catch (ButtplugClientConnectorException e)
+            {
+
+            }
+            catch (ButtplugException e)
+            {
+
+            }
+        }
+    }
+}
