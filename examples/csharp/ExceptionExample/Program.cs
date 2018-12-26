238d5d120317e7708ba718dd44e2af4d916f0e09
Kyle Machulis <kyle@machul.is>
2018-12-26 11:54:33 -0800

    Add event example and fix columns for C#

--- examples/csharp/ExceptionExample/Program.cs
+++ examples/csharp/ExceptionExample/Program.cs
@@ -8,9 +8,9 @@ namespace ExceptionExample
         static void Main(string[] args)
         {
             // In CSharp, we've got the 5 main exception classes, plus
-            // ButtplugClientConnectorException. All of these are children of ButtplugException, so
-            // catching ButtplugException will catch all Buttplug errors but let all other system
-            // Exceptions bubble up.
+            // ButtplugClientConnectorException. All of these are children of
+            // ButtplugException, so catching ButtplugException will catch all
+            // Buttplug errors but let all other system Exceptions bubble up.
             try
             {
             }
