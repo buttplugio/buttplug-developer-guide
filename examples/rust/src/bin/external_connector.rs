b30f345b51386dd23a91da32fcea96fd51f54551
Melody Horn <melody@boringcactus.com>
2020-09-16 15:00:17 -0600

    add Rust example for external connector

---
+++ examples/rust/src/bin/external_connector.rs
@@ -0,0 +1,17 @@
+use buttplug::{
+    client::ButtplugClient,
+    connector::{ButtplugRemoteConnector, ButtplugWebsocketClientTransport},
+    core::messages::serializer::ButtplugClientJSONSerializer,
+};
+
+#[async_std::main]
+async fn main() {
+    // To create a Websocket Connector, you need the websocket address and some generics fuckery.
+    let connector: ButtplugRemoteConnector<_, ButtplugClientJSONSerializer, _, _> = ButtplugRemoteConnector::new(
+        ButtplugWebsocketClientTransport::new_insecure_connector("ws://localhost:12345/buttplug")
+    );
+
+    let (_client, _event_stream) = ButtplugClient::connect("Example Client", connector)
+        .await
+        .expect("Can't connect to Buttplug Server, exiting!");
+}
